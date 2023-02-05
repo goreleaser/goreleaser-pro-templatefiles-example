4cd621dac4e4f03cc398b79bb262737ecdc6af4e
Carlos A Becker <caarlos0@users.noreply.github.com>
2023-02-05 16:25:27 -0300

    fix: release file
    Signed-off-by: Carlos A Becker <caarlos0@users.noreply.github.com>

--- .github/release.tpl.sh
+++ .github/release.tpl.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 echo "Running {{ .Tag }}"
+tag={{.Tag}}
+name={{.ProjectName}}
 {{ range .Artifacts }}
 {{- if eq .Goos "linux" }}
-{{- if eq .Type.String "Binary" }}
+{{- if eq .Type.String "Archive" }}
 {{- if eq .Goarch "amd64" }}
-if [[ "x86_64" = $(uname -m) ]]; then
-	echo "Executing {{ .Name }} ({{ .Path }})..."
-  {{ .Path }}
+if [[ "Linux" = $(uname -s) ]]; then
+	if [[ "x86_64" = $(uname -m) ]]; then
+		wget "https://github.com/caarlos0/goreleaser-pro-templatefiles-example/releases/download/$tag/{{.Name}}" -O "{{.Name}}"
+		tar xvzf "{{.Name}}"
+		{{- range .Extra.Binaries }}
+		./{{ . }}
+		{{- end }}
+	fi
 fi
 {{- end }}
 {{- end }}
